26a26125a7d8e00ad40181b778e54db972d32ac3
soong-e <sksuwjd@naver.com>
2023-02-10 18:05:46 +0900

    feat. remove manage attr, add ft_Exit

---
+++ src/manage/manage_error.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_exit(char *errmsg)
+{
+	printf("%s\n", errmsg);
+	exit(1);
+}
---
+++ src/manage/manage_resource.c
@@ -0,0 +1,11 @@
+#include "miniRT.h"
+
+void	ft_free_splitted(char **chars)
+{
+	int	i;
+
+	i = 0;
+	while (chars[i] != NULL)
+		free(chars[i]);
+	free(chars);
+}
