5347b740edb790bc10a053609617010bbcd7077d
soong-e <sksuwjd@naver.com>
2023-03-02 21:51:33 +0900

    feat. free lights

--- src/manage/manage_resource.c
+++ src/manage/manage_resource.c
@@ -10,7 +10,17 @@ void	ft_free_splitted(char **chars)
 	free(chars);
 }
 
-void	free_objs(t_obj *objs)
+void	free_resources(t_meta *meta)
 {
-	free(objs);
+	t_light *head;
+	t_light *temp;
+
+	free(meta->objs);
+	head = meta->light;
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
