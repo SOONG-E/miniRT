25e55b0868eb3e85b7a20fa94441f95e8eb56133
soong-e <sksuwjd@naver.com>
2023-03-02 11:37:48 +0900

    feat. add key hook for moving cam, light

--- src/manage/manage_mlx.c
+++ src/manage/manage_mlx.c
@@ -8,8 +8,48 @@ void	put_pixel(t_mlx *mlx, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void open_screen(t_mlx mlx)
+static void	change_coor(int key, t_vec *coor)
 {
+	if (key == KEY_ARROW_UP)
+		coor->z -= 1;
+	else if (key == KEY_ARROW_DOWN)
+		coor->z += 1;
+	else if (key == KEY_ARROW_LEFT)
+		coor->x -= 1;
+	else if (key == KEY_ARROW_RIGHT)
+		coor->x += 1;
+	else if (key == KEY_SHIFT)
+		coor->y += 1;
+	else if (key == KEY_CTRL)
+		coor->y -= 1;
+}
+
+static int	binding_key_events(int key, t_meta *meta)
+{
+	if (meta->mode != -1 && key == KEY_Z)
+	{
+		meta->mode = -1;
+		return (TRUE);
+	}
+	if (meta->mode == -1)
+	{
+		if (key == KEY_L)
+			meta->mode = KEY_L;
+		else if (key == KEY_C)
+			meta->mode = KEY_C;
+		return (TRUE);
+	}
+	else if (meta->mode == KEY_L)
+		change_coor(key, &(meta->light.coor));
+	else if (meta->mode == KEY_C)
+		change_coor(key, &(meta->cam.coor));
+	ray_tracing(meta);
+	return (TRUE);
+}
+
+void open_screen(t_mlx mlx, t_meta *meta)
+{
+	mlx_key_hook(mlx.win_ptr, binding_key_events, meta);
 	mlx_hook(mlx.win_ptr, 2, 1L << 0, &key_event, &mlx);
 	mlx_hook(mlx.win_ptr, 17, 0, &exit_hook, 0);
 	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img.img_ptr, 0, 0);
