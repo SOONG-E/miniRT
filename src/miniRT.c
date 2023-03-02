ee9b2d6ff207a16e797f9fa98d435bfedf30b41f
soong-e <sksuwjd@naver.com>
2023-03-02 13:21:03 +0900

    fix. change location that open window

--- src/miniRT.c
+++ src/miniRT.c
@@ -3,28 +3,12 @@
 int main(int ac, char **av)
 {
 	t_meta	meta;
-	
+
 	if (ac != 2)
 		ft_exit("miniRT need only one argument");
 	parse_line(av[1], &meta);
-	ray_tracing(meta);
-
+	ray_tracing(&meta);
+	open_screen(&(meta.mlx), &meta);
+	
 	return (0);
 }
-
-
-/*
-
-0. mlx창 띄우기
-	mlx 구조체 {
-		mlxx
-		window
-		img
-
-	}
-1. 카메라 시점에서 보이는 코너 점을 구함
-2. 배경을 먼저 그림
-3. 오브젝트를 그림
-4. 바닥/ 빛/ 그림자 그림
-
-*/
