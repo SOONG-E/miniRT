f0e7a73d85b90aa3e42d10142973b766a1ddcabc
soong-e <sksuwjd@naver.com>
2023-03-02 21:50:49 +0900

    feat. add keyhook for direction of camera
    add keyhook, arrange functions

--- src/manage/manage_keyhook.c
+++ src/manage/manage_keyhook.c
@@ -12,6 +12,62 @@ int	key_event(int key, t_meta *meta)
 
 int	exit_hook(t_meta *meta)
 {
-	free_objs(meta->objs);
+	free_resources(meta);
 	exit(0);
+}
+
+static void	change_coor(int key, t_vec *coor)
+{
+	if (key == KEY_ARROW_UP)
+		coor->z -= 1;
+	else if (key == KEY_ARROW_DOWN)
+		coor->z += 1;
+	else if (key == KEY_ARROW_LEFT)
+		coor->x -= 1;
+	else if (key == KEY_ARROW_RIGHT)
+		coor->x += 1;
+	else if (key == KEY_SHIFT)
+		coor->y += 1;
+	else if (key == KEY_CTRL)
+		coor->y -= 1;
+}
+
+static void	change_vec(int key, t_vec *coor)
+{
+	if (key == KEY_ARROW_UP)
+		coor->z -= 0.05;
+	else if (key == KEY_ARROW_DOWN)
+		coor->z += 0.05;
+	else if (key == KEY_ARROW_LEFT)
+		coor->x -= 0.05;
+	else if (key == KEY_ARROW_RIGHT)
+		coor->x += 0.05;
+	else if (key == KEY_SHIFT)
+		coor->y += 0.05;
+	else if (key == KEY_CTRL)
+		coor->y -= 0.05;
+	*coor = clamp(*coor, -1, 1);
+}
+
+int	binding_key_events(int key, t_meta *meta)
+{
+	if (meta->mode != -1 && key == KEY_Z)
+	{
+		meta->mode = -1;
+		return (TRUE);
+	}
+	if (meta->mode == -1)
+	{
+		if (key == KEY_D)
+			meta->mode = KEY_D;
+		else if (key == KEY_C)
+			meta->mode = KEY_C;
+		return (TRUE);
+	}
+	else if (meta->mode == KEY_D)
+		change_vec(key, &(meta->cam.vec));
+	if (meta->mode == KEY_C)
+		change_coor(key, &(meta->cam.coor));
+	ray_tracing(meta);
+	return (TRUE);
 }
--- src/manage/manage_mlx.c
+++ src/manage/manage_mlx.c
@@ -8,45 +8,6 @@ void	put_pixel(t_mlx *mlx, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-static void	change_coor(int key, t_vec *coor)
-{
-	if (key == KEY_ARROW_UP)
-		coor->z -= 1;
-	else if (key == KEY_ARROW_DOWN)
-		coor->z += 1;
-	else if (key == KEY_ARROW_LEFT)
-		coor->x -= 1;
-	else if (key == KEY_ARROW_RIGHT)
-		coor->x += 1;
-	else if (key == KEY_SHIFT)
-		coor->y += 1;
-	else if (key == KEY_CTRL)
-		coor->y -= 1;
-}
-
-static int	binding_key_events(int key, t_meta *meta)
-{
-	if (meta->mode != -1 && key == KEY_Z)
-	{
-		meta->mode = -1;
-		return (TRUE);
-	}
-	if (meta->mode == -1)
-	{
-		if (key == KEY_L)
-			meta->mode = KEY_L;
-		else if (key == KEY_C)
-			meta->mode = KEY_C;
-		return (TRUE);
-	}
-	else if (meta->mode == KEY_L)
-		change_coor(key, &(meta->light.coor));
-	else if (meta->mode == KEY_C)
-		change_coor(key, &(meta->cam.coor));
-	ray_tracing(meta);
-	return (TRUE);
-}
-
 void open_screen(t_mlx *mlx, t_meta *meta)
 {
 	mlx_key_hook(mlx->win_ptr, binding_key_events, meta);
