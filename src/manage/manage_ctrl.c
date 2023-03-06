33d2d99c22abef84bc709802f9fc1d703560b7e9
soong-e <sksuwjd@naver.com>
2023-03-06 21:38:14 +0900

    feat. control objects, print manual

---
+++ src/manage/manage_ctrl.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   manage_ctrl.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/06 21:37:19 by yujelee           #+#    #+#             */
+/*   Updated: 2023/03/06 21:37:37 by yujelee          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+
+static void	select_light(t_meta *meta)
+{
+	meta->ctrl.light = meta->ctrl.light->next;
+	if (meta->ctrl.light == NULL)
+		meta->ctrl.light = meta->light;
+}
+
+static void	select_object(t_meta *meta)
+{
+	int	idx;
+
+	idx = meta->ctrl.obj->num + 1;
+	if (idx == meta->obj_num)
+		idx = 0;
+	meta->ctrl.obj = &(meta->objs[idx]);
+}
+
+void	select_obj(t_meta *meta)
+{
+	if (meta->ctrl.category == KEY_L)
+		select_light(meta);
+	else if (meta->ctrl.category == KEY_O)
+		select_object(meta);
+}
+
+void	set_category(int key, t_meta *meta)
+{
+	if (key == KEY_L)
+	{
+		meta->ctrl.category = KEY_L;
+		meta->ctrl.light = meta->light;
+	}
+	else if (key == KEY_C)
+		meta->ctrl.category = KEY_C;
+	else if (key == KEY_O)
+	{
+		meta->ctrl.category = KEY_O;
+		meta->ctrl.obj = &(meta->objs[0]);
+	}
+}
--- src/miniRT.c
+++ src/miniRT.c
@@ -6,11 +6,22 @@
 /*   By: yujelee <yujelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:29:31 by yujelee           #+#    #+#             */
-/*   Updated: 2023/03/06 16:29:32 by yujelee          ###   ########seoul.kr  */
+/*   Updated: 2023/03/06 21:36:04 by yujelee          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+void	print_manual(void)
+{
+	printf("press L for controlling lights\n");
+	printf("press C for controlling camera\n");
+	printf("press O for controlling objects\n");
+	printf("press Z for reselecing category\n");
+	printf("press M for moving object\n");
+	printf("press D for changing direction of object\n");
+	printf("control key : ←, ↑, →, ↓, shift, ctrl \n");
+}
 
 int	main(int ac, char **av)
 {
@@ -20,6 +31,7 @@ int	main(int ac, char **av)
 		ft_exit("miniRT need only one argument");
 	parse_line(av[1], &meta);
 	ray_tracing(&meta);
+	print_manual();
 	open_screen(&(meta.mlx), &meta);
 	return (0);
 }
