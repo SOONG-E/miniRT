7b4688da9a81d9d062b26dffda3dd1a285c71f5f
soong-e <sksuwjd@naver.com>
2023-03-06 16:12:03 +0900

    fix. fix typo

--- src/vec/calculate_vec.c
+++ src/vec/calculate_vec.c
@@ -38,8 +38,8 @@ t_vec	vec_cross(t_vec v1, t_vec v2)
 {
 	t_vec	out;
 
-	out.x = v1.y * v2.z + v1.z * v2.y;
-	out.y = v1.z * v2.x + v1.x * v2.z;
-	out.z = v1.x * v2.y + v1.y * v2.x;
+	out.x = v1.y * v2.z - v1.z * v2.y;
+	out.y = v1.z * v2.x - v1.x * v2.z;
+	out.z = v1.x * v2.y - v1.y * v2.x;
 	return (out);
 }
